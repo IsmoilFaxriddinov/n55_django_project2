85e659b6d55488d00ec8c7b48aaf17873a07a7bd
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-08-08 19:01:36 +0500

    commit

--- Desktop/bolani_onasi_uchun/core/wsgi.py
+++ Desktop/bolani_onasi_uchun/core/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for core project.
+WSGI config for core projects.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.prod')
 
 application = get_wsgi_application()
+
+
